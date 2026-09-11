7eac42229722a2f9240775640014af618951b7c1
GiacomoVisin <killerboss1150@gmail.com>
2026-09-11 13:22:33 +0200

    add icons

--- Models/movie.php
+++ Models/movie.php
@@ -9,29 +9,28 @@ class Movie
     public $rating;
     public array $genres;
 
-    public function __construct($title, $director, $date, $rating, array $genres)
+    public $urlImg;
+
+    public function __construct($title, $director, $date, $rating, array $genres, $urlImg)
     {
         $this->title = $title;
         $this->director = $director;
         $this->date = $date;
         $this->rating = $rating;
         $this->genres = $genres;
+        $this->urlImg = $urlImg;
 
     }
 
 
-    public function GetGenre()
-    {
-        foreach ($this->genres as $genre) {
-            echo "[" . $genre->genreName .  "]". "," ;
-        }
+    public function getGenre()
+{
+    $genreList = [];
+    foreach ($this->genres as $genre) {
+        $genreList[] = "[" . $genre->genreName . "]";
     }
-
-    public function getMovieDirector()
-    {
-        return $this->director;
-
-    }
+    return implode(", ", $genreList);
+}
 
 
 
