744994178262bdd07f39c956afe742192565a19c
GiacomoVisin <killerboss1150@gmail.com>
2026-09-14 11:11:13 +0200

    adding some style and one more film

--- db.php
+++ db.php
@@ -8,17 +8,21 @@ require_once ("./Models/Movie.php");
 $fantasyGenre = new Genre("Fantasy");
 $sciFiGenre = new Genre("Sci-Fi");
 $actionGenre = new Genre("Action");
+$comedyGenre = new Genre("Comedy");
 
 // Date
 $datePacificRim = new Date("30", "10", "2018");
 $dateAvengers = new Date("20", "11", "2026");
+$dateSpiderman = new Date("18", "09", "2004");
 
 // Lista Film
 $movies = [
 
     $PacificRim = new Movie("Pacific Rim", "John McTiernan", $dateAvengers, "7/10", [$sciFiGenre, $fantasyGenre],"https://musicart.xboxlive.com/7/a26b5100-0000-0000-0000-000000000002/504/image.jpg"),
 
-    $Avengers = new Movie("Avengers", "Jhon Smith", $datePacificRim, "9/10", [$fantasyGenre, $actionGenre],"https://wallpapers.com/images/hd/marvel-avengers-team-battle-scene-7ghqv7ih0domruuq.jpg")
+    $Avengers = new Movie("Avengers", "Jhon Smith", $datePacificRim, "9/10", [$fantasyGenre, $actionGenre],"https://wallpapers.com/images/hd/marvel-avengers-team-battle-scene-7ghqv7ih0domruuq.jpg"),
+
+    $Spiderman = new Movie("Spiderman", "Stan lee", $dateSpiderman, "10/10", [$comedyGenre, $actionGenre],"https://cdn.mos.cms.futurecdn.net/8aX68dFkFBNPS7anGtg9EA.jpg")
 ];
 
 ?>
--- index.php
+++ index.php
@@ -7,45 +7,55 @@ require_once("./db.php");
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+
+<!--  font-style   -->
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Oswald&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Caacupe+One&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Oswald&display=swap');
+</style>
+
+<!-- meta-data -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP-OOP-MOVIE</title>
+
+<!-- bootstrap -->    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
+    
+<!-- css -->   
     <link rel="stylesheet" href="index.css">
+
 </head>
 
 <body>
-    <h1>PHP-OOP-MOVIE</h1>
 
+    <h1 class="text-center mb-5">PHP-OOP-MOVIE</h1>
 
-    <div class="container">
+    <div class="container d-flex">
     <?php
     foreach ($movies as $movie) {
-        echo "<div class='card mb-3 d-flex'>
-            <div class='card-header'>
-                <img src='$movie->urlImg'>
-                <span>{$movie->title}</span>
-            </div>
-            <div class='card-body'>
-                <p><strong>Regia:</strong> {$movie->director}</p>
-            </div>
-            <div class='card-footer'>
-                <span><strong>Generi:</strong> {$movie->getGenre()}</span>
-                <p> $movie->rating <i class='bi bi-star-fill'></i> </p> 
-            </div>
-        </div>";
+        echo "<div class='card mb-3 mx-3 d-flex' style='width: 20rem';>
+                <img src='$movie->urlImg' class='card-img-top' style='width: 20rem ; height: 12rem'>
+                    <div class='card-header bg-dark' >
+                        <span class='text-white'>{$movie->title}</span>
+                    </div>
+                    <div class='card-body'>
+                        <p><strong>Regia:</strong> {$movie->director}</p>
+                    </div>
+                    <div class='card-footer'>
+                        <span><strong>Generi:</strong> {$movie->getGenre()}</span>
+                        <hr>
+                        <p>  Rating: $movie->rating <i class='bi bi-star-fill'></i> </p> 
+                    </div>
+             </div>";
     }
     ?>
 </div>
-
-
-
 
 </body>
 
